e1aebb7591be1edbc30f76b665778db6d48fb346
BudaViktor <vicktor0095@gmail.com>
2022-04-24 13:26:06 +0200

    Adatbázis leírása és szerkezete

--- Docs/Description/database_data.sql
+++ Docs/Description/database_data.sql
@@ -1,6 +1,6 @@
-Insert into persons (ID, name, password) values 
-    (1, 'teszt1', 'teszt1'),
-    (2, 'teszt2', 'teszt2');
+Insert into persons (ID, name, password, email, postcode, country, county, city, active) values 
+    (1, 'teszt1', 'teszt1', 'teszt1@valami.hu', 4032, 'Magyarország', 'Hajdú-Bihar', 'Debrecen', true),
+    (2, 'teszt2', 'teszt2', 'teszt2@valami.hu', 1182, 'Magyarország', 'Pest', 'Budapest', true);
 	
 Insert into categories(ID, denomination) values
     (1, 'élelmiszer'),
@@ -11,24 +11,24 @@ Insert into categories(ID, denomination) values
     (6, 'édesség/nassolnivaló'),
 	(7, 'egyéb');
 	
-Insert into registers (ID, personsID, denomination, amount, dates, categoriesID) values 
-    (1, 1, 'bevétel', 100000, '2018-01-01', NULL),
-    (2, 1, 'kiadás', 250, '2018-01-03', 6),
-    (3, 1, 'kiadás', 5000, '2018-01-04', 1),
-    (4, 1, 'kiadás', 15432, '2018-01-07', 3),
-    (5, 1, 'kiadás', 657, '2018-01-14', 2),
-    (6, 1, 'kiadás', 988, '2018-01-18', 5),
-    (7, 1, 'bevétel', 155000, '2018-01-20', NULL),
-    (8, 1, 'kiadás', 324, '2018-02-02', 1),
-    (9, 1, 'kiadás', 3200, '2018-02-09', 1),
-    (10, 1, 'kiadás', 320, '2018-02-16', 2),
-    (11, 2, 'bevétel', 200000, '2018-01-02', NULL),
-    (12, 2, 'kiadás', 65000, '2018-01-03', 4),
-    (13, 2, 'kiadás', 980, '2018-01-05', 1),
-    (14, 2, 'kiadás', 7845, '2018-01-09', 3),
-    (15, 2, 'kiadás', 450, '2018-01-15', 6),
-    (16, 2, 'kiadás', 555, '2018-01-27', 7),
-    (17, 2, 'bevétel', 35000, '2018-02-02', NULL),
-    (18, 2, 'kiadás', 10345, '2018-02-08', 5),
-    (19, 2, 'kiadás', 670, '2018-02-13', 1),
-    (20, 2, 'kiadás', 871, '2018-02-19', 7);
+Insert into registers (ID, personsID, amount, dates, categoriesID) values 
+    (1, 1, 100000, '2018-01-01', NULL),
+    (2, 1, -250, '2018-01-03', 6),
+    (3, 1, -5000, '2018-01-04', 1),
+    (4, 1, -15432, '2018-01-07', 3),
+    (5, 1, -657, '2018-01-14', 2),
+    (6, 1, -988, '2018-01-18', 5),
+    (7, 1, 155000, '2018-01-20', NULL),
+    (8, 1, -324, '2018-02-02', 1),
+    (9, 1, -3200, '2018-02-09', 1),
+    (10, 1, -320, '2018-02-16', 2),
+    (11, 2, 200000, '2018-01-02', NULL),
+    (12, 2, -65000, '2018-01-03', 4),
+    (13, 2, -980, '2018-01-05', 1),
+    (14, 2, -7845, '2018-01-09', 3),
+    (15, 2, -450, '2018-01-15', 6),
+    (16, 2, -555, '2018-01-27', 7),
+    (17, 2, 35000, '2018-02-02', NULL),
+    (18, 2, -10345, '2018-02-08', 5),
+    (19, 2, -670, '2018-02-13', 1),
+    (20, 2, -871, '2018-02-19', 7);
--- Docs/Description/database_table.sql
+++ Docs/Description/database_table.sql
@@ -1,7 +1,13 @@
 CREATE TABLE persons (
     ID int NOT NULL AUTO_INCREMENT,
-    name  varchar(255) NOT NULL,
+    name  varchar(50) NOT NULL,
 	password text NOT NULL,
+	email text NOT NULL,
+	postcode int(4) NOT NULL,
+	country varchar(30) NOT NULL,
+	county varchar(30) NOT NULL,
+	city varchar(30) NOT NULL,
+	active boolean DEFAULT true,
     PRIMARY KEY (ID)
 	);
 
@@ -14,11 +20,10 @@ CREATE TABLE categories (
 CREATE TABLE registers (
     ID int NOT NULL AUTO_INCREMENT,
     personsID int NOT NULL,
-    denomination  varchar(255) NOT NULL,
     amount int NOT NULL,
 	dates DATE NOT NULL,
 	categoriesID int,
     PRIMARY KEY (ID),
-	FOREIGN KEY (`personsID`) REFERENCES `persons`(`ID`),
-	FOREIGN KEY (`categoriesID`) REFERENCES `categories`(`ID`)
+	FOREIGN KEY (`personsID`) REFERENCES `persons`(`ID`) ON DELETE CASCADE,
+	FOREIGN KEY (`categoriesID`) REFERENCES `categories`(`ID`) ON DELETE CASCADE
 	);
