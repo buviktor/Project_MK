1d761cc01572fbda691047c200ee35b3ca254bb6
Balu <uhrinbaluukagmail.com>
2022-04-30 21:37:55 +0200

    listázás

--- Client/Web/login.js
+++ Client/Web/login.js
@@ -1,12 +1,11 @@
 // Lekérdezés
-function Allpost(e) {
-    e.preventDefault();
-    const url = 'http://localhost:5000/AllPost';
-    const token = 'Bearer: ' + sessionStorage.token
+window.addEventListener("load", function Allpost(){
+    const url = 'http://localhost:5000/Users/AllPost';
+    // const token = 'Bearer: ' + sessionStorage.token
     fetch(url, {
-        method: 'GET',
+        method: 'POST',
         headers: {
-            'Authorization': token
+            'Content-type': 'application/json;charset=utf-8'
         },
         body: JSON.stringify({
             "osszeg": "00",
@@ -20,18 +19,33 @@ function Allpost(e) {
         ok = response.ok
         return response.json()
     })
+        .catch(err => console.log(err));
+})
+
+
+
+// Lekérdezés
+window.addEventListener("load", function Allpost1(){
+    const url = 'http://localhost:5000/Users/AllPost';
+     const token = 'Bearer: ' + sessionStorage.token
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': token
+        }
+    })
+    .then((response) => {
+        ok = response.ok
+        return response.json()
+    })
     .then(json => {
         if (ok) {
-            lista.innerHTML = "";
             json.forEach(p => {
                 lista.innerHTML += "<td>" + p.amount + " " + p.date + "</td>"
             })
         } else {
             document.getElementById("uzenet").innerHTML = json.message
         }
-    }
-    )
+    })
         .catch(err => console.log(err));
-}
-
-Allpost();
+})
