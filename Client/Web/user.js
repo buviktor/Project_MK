59502dee9b04450dcbbfa83d462c7b46c1ac7543
BudaViktor <vicktor0095@gmail.com>
2022-06-01 23:14:24 +0200

    javítások félig

--- Client/Web/user.js
+++ Client/Web/user.js
@@ -1,20 +1,28 @@
 // Lekérdezés
-window.addEventListener("load", function Allpost() {
-    const url = 'http://localhost:5000/user/all/' + sessionStorage.id;
+function Allpost() {
+    const url = 'http://localhost:5000/user/all/1' /*+ sessionStorage.id*/;
     const token = 'Bearer: ' + sessionStorage.token
     const lista= document.getElementById("lista");
-    fetch(url, {
-        method: 'GET'
-    })
-        .then((response) => response.json())
-        console.log(json())
 
-        lista.innerHTML = ""; 
-        json.forEach(f => { lista.innerHTML += "<tr>" +"<td>" + f.amount + "</td>" + "<td>" + f.denomination + "</td>" + "<td>" + f.date + "</td>" + "</tr>"  
-
-    })
+    fetch(url, { 
+        method: "GET",
+        headers: {'Authorization': token}
+      })
+        .then(response => {ok = response.ok; return response.json()})
+            
+        .then(json => {lista.innerHTML = json})
+        /*.then((json) => {
+            if (ok) {
+                lista.innerHTML = "";
+                json.forEach(f => { lista.innerHTML = f
+                    /*+= "<tr>" +"<td>" 
+                        + f.amount + "</td>" 
+                        + "<td>" + f.denomination 
+                        + "</td>" + "<td>" + f.date + "</td>" + "</tr>"  */
+        /*})}})*/
+        
         .catch (err => console.log(err));
-})
+}
 
-
+Allpost();
 
